f0e974c8d104e5e3061c50b1952365e236c6f8a5
BiroMarton <rezoner9@gmail.com>
2017-07-12 07:52:05 +0200

    Exercise completed.

---
+++ Search/Search/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Search
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Search = new System.Windows.Forms.TextBox();
+            this.Text = new System.Windows.Forms.TextBox();
+            this.Matches = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Pattern = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(163, 407);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(181, 20);
+            this.Search.TabIndex = 0;
+            // 
+            // Text
+            // 
+            this.Text.Location = new System.Drawing.Point(12, 49);
+            this.Text.Multiline = true;
+            this.Text.Name = "Text";
+            this.Text.Size = new System.Drawing.Size(363, 291);
+            this.Text.TabIndex = 1;
+            // 
+            // Matches
+            // 
+            this.Matches.Location = new System.Drawing.Point(403, 49);
+            this.Matches.Multiline = true;
+            this.Matches.Name = "Matches";
+            this.Matches.Size = new System.Drawing.Size(373, 291);
+            this.Matches.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(359, 407);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(30, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(416, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Matches.";
+            // 
+            // Pattern
+            // 
+            this.Pattern.AutoSize = true;
+            this.Pattern.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Pattern.Location = new System.Drawing.Point(96, 407);
+            this.Pattern.Name = "Pattern";
+            this.Pattern.Size = new System.Drawing.Size(61, 20);
+            this.Pattern.TabIndex = 6;
+            this.Pattern.Text = "Pattern";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 460);
+            this.Controls.Add(this.Pattern);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Matches);
+            this.Controls.Add(this.Text);
+            this.Controls.Add(this.Search);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Search;
+        private System.Windows.Forms.TextBox Text;
+        private System.Windows.Forms.TextBox Matches;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Pattern;
+    }
+}
+
---
+++ Search/Search/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Search
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Matches.Clear();
+            Regex regex = new Regex(Search.Text);
+            string[] content = Text.Text.Split(' ');
+
+            foreach (string word in content) {
+                if (regex.IsMatch(word))
+                {
+                    Matches.Text += word + ", ";
+                }
+            }
+
+       
+        }
+    }
+}
